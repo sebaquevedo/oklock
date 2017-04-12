c577ee09fc6ac2439deac530e1c466bd8f56ce53
sebastian quevedo <sebaqd91@gmail.com>
2017-04-12 10:51:41 -0300

    template added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,20 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap.min.js
+//= require jquery-library.js
+//= require markerclusterer.min.js
+//= require modernizr-2.8.3-respond-1.4.2.min
+//= require infobox.js
+//= require map.js
+//= require jquery.countdown.js
+//= require owl.carousel.min.js
+//= require scrollbar.min.js
+//= require prettyPhoto.js
+//= require jquery-ui.js
+//= require gmap3.js
+//= require readmore.js 
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap.js
