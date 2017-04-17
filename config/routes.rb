e4b351057894cc536c8b0910e47532466b561c3c
sebastian quevedo <sebaqd91@gmail.com>
2017-04-17 19:27:05 -0300

    landing page + cambios

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 	devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'users/registrations' }
 	
 	get 'home/index'
-	root 'home#index'
+	root 'home#landing'
 	  authenticated :user do
     root to: 'welcome#index', as: :authenticated_root
   end
 
-  root to: 'landing#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
