0c06f2f5ecdeb3be48dedb41d20b66368a989486
sebastian quevedo <sebaqd91@gmail.com>
2017-04-27 18:32:26 -0300

    login views added bootstrap.min.css removed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -22,7 +22,7 @@ Rails.application.config.assets.precompile += %w( jquery-ui.js )
 Rails.application.config.assets.precompile += %w( readmore.js )
 Rails.application.config.assets.precompile += %w( gmap3.js )
 Rails.application.config.assets.precompile += %w( main.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
 Rails.application.config.assets.precompile += %w( normalize.css )
 
 
